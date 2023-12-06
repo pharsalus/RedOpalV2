8130e21e715f7ba0c55e31ebeb9f47cb7c5b08d3
John Fives <jfives74@gmail.com>
2023-12-06 19:29:51 +1100

    Design Page 1 complete

---
+++ RedOpalV2/Model/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedOpalV2.Model
+{
+    internal class Staff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RedOpalV2/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RedOpalV2.Model;//Gets data
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedOpalV2.ViewModel
+{
+    internal class MainPageViewModel
+    {
+        //MODAL
+        // Staff Staff { get; set; } - just a test
+
+        //Observable
+        //[ObservableObject]
+        //List<Staff> Staff { get; set; }
+    }
+}
